eb6be5816e98eaadfe2bb74467c29a5af2012f13
RAKIB-ALI-018 <rakibali99580@gmail.com>
2026-07-25 07:23:19 +0530

    feat: add cookie-parser middleware to express app

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,10 @@
 //server creation
 
 const express = require("express")
+const cookieParser = require("cookie-parser")
 require("dotenv").config()
 
 const app = express()
+app.use(express.json())
 
 module.exports = app
