41737d9ba325b71419e099080f6276e450f69562
RAKIB-ALI-018 <rakibali99580@gmail.com>
2026-07-31 17:32:31 +0530

    feat: implement authentication routes and middleware for user registration and login

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,4 +8,8 @@ const cookieParser = require("cookie-parser")
 const app = express()
 app.use(express.json())
 
+//* Routes
+const authRoutes = require("./routes/auth.routes")
+app.use("/api/auth", authRoutes)
+
 module.exports = app
--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -2,37 +2,98 @@ const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcryptjs")
 
-async function registerUser(req, res){
-    const {username, email, password} = req.body
+async function registerUser(req, res) {
+    const { username, email, password } = req.body
 
     const isUserAlreadyExist = await userModel.findOne({
-        $or:[
-            {email},
-            {username}
+        $or: [
+            { email },
+            { username }
         ]
     })
 
-    if(isUserAlreadyExist){
+    if (isUserAlreadyExist) {
         return res.status(409).json({
-            message:`User already exist with this ${isUserAlreadyExist.username === username? username : email} `
+            message: `User already exist with this ${isUserAlreadyExist.username === username ? username : email} `
         })
     }
 
     const hash = await bcrypt.hash(password, 10)
 
     const user = await userModel.create({
-        username, email, password
+        username, email, password:hash
     })
 
     const token = jwt.sign({
         id: user._id,
         username: user.username
-    }, process.env.JWT_SECRET, {expiresIn:"3d"})
+    }, process.env.JWT_SECRET_KEY, { expiresIn: "3d" })
+
+    res.cookie("token", token)
+
+    return res.status(201).json({
+        message: "User registered successfully",
+        user:{
+            id:user._id,
+            username:user.username,
+            email:user.email
+        }
+    })
 
 }
 
-async function loginUser(req, res){
+async function loginUser(req, res) {
+    const { username, email, password } = req.body
+
+    const user = await userModel.findOne({
+        $or: [
+            { email }, { username }
+        ]
+    })
+    if (!user) {
+        return res.status(400).json({
+            message: "Invalid Credentials." //* Task-2
+        })
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+        return res.status(400).json({
+            message: "Invalid Credentials."
+        })
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+        username: user.username,
+        email: user.email
+    }, process.env.JWT_SECRET_KEY, { expiresIn: "3d" })
+
+    res.cookie("token", token)
+
+    return res.status(201).json({
+        message: "user logged in successfully.",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+
+
 
 }
 
-module.exports = {registerUser, loginUser}
+async function getMe(req,res){
+    const user = await userModel.findById()
+
+}
+
+module.exports = { registerUser, loginUser, getMe }
+
+/** Task2 Answer: Agar tum !user case mein 
+ * "User not found" bolte, aur wrong password 
+ * case mein "Invalid Password" bolte, toh 
+ * ek attacker easily pata laga sakta hai ki: Konsa email/username 
+ * system mein exist karta hai Konsa nahi karta
+*/ 
