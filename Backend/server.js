b1abd2846e244f77de22bc719093d450e13d1306
RAKIB-ALI-018 <rakibali99580@gmail.com>
2026-08-07 04:57:59 +0530

    feat: update server and app initialization, enhance CORS configuration, and serve frontend in production

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,12 @@
-//start the server
-//connect to databse
+require("dotenv").config();
 
-const app = require("./src/app")
-const connectToDB = require("./src/config/database")
+const app = require("./src/app");
+const connectToDB = require("./src/config/database");
 
-connectToDB()
+connectToDB();
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000.");   
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,29 +1,33 @@
-//server creation
+require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
 
-require("dotenv").config()
-const express = require("express")
-const cookieParser = require("cookie-parser")
+const app = express();
 
-//*
-const cors= require("cors")
-//*
+app.use(express.json());
+app.use(cookieParser());
 
-
-const app = express()
-app.use(express.json())
-app.use(cookieParser()) 
-
-//*
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
-//*
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true
+}));
 
-//* Routes
-const authRoutes = require("./routes/auth.routes")
-const songRoutes = require("./routes/song.routes")
-app.use("/api/auth", authRoutes)
-app.use("/api/songs", songRoutes)
+// Routes
+const authRoutes = require("./routes/auth.routes");
+const songRoutes = require("./routes/song.routes");
 
-module.exports = app
+app.use("/api/auth", authRoutes);
+app.use("/api/songs", songRoutes);
+
+// Serve frontend only in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../public")));
+
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+}
+
+module.exports = app;
