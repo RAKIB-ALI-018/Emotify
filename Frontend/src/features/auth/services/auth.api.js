f5c61c652b05083b31e8372324a7a27355132c23
RAKIB-ALI-018 <rakibali99580@gmail.com>
2026-08-07 05:17:21 +0530

    feat: update API configuration to use environment variables for base URL

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -3,9 +3,11 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:"http://localhost:3000",
-    withCredentials:true
-})
+    baseURL: import.meta.env.DEV
+        ? "http://localhost:3000"
+        : "",
+    withCredentials: true,
+});
 
 export async function register({email, username, password}){
     const response = await api.post('/api/auth/register',
